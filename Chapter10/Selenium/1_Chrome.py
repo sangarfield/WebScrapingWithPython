9d034b801d86946a1b1220d5db5a02c6379bf6f0
Kevin Liu <Kevin Liu>
2016-05-19 23:31:14 +0800

    selenium

--- Chapter10/Selenium/1_Chrome.py
+++ Chapter10/Selenium/1_Chrome.py
@@ -2,5 +2,5 @@ from selenium import webdriver
 
 url = r"www.baidu.com"
 broswer = webdriver.Chrome()
-broswer.get(url)
+broswer.get('http://www.zhihu.com')
 print(broswer.title)
---
+++ Chapter10/Selenium/2_Search_PythonOrg.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keysj
+url = 'http://www.python.org'
+
+broswer = webdriver.Chrome()
+broswer.get(url)
+assert "Python" in broswer.title
+ele = broswer.find_element_by_id("id-search-field")
+ele.send_keys("sorted")
+ele.send_keys(Keys.RETURN)
+print(broswer.page_source)
